89b0a4dff0586f4f6e358e8853e9dee12d5a9680
Irfan-Ansari-au25 <irfan398649@gmail.com>
2022-01-15 12:20:54 +0530

    Created Routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
 
 function App() {
   return (
-    <div>
-      <h2>Hello world</h2>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
